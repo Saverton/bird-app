4435d4d8b442122cd8b73c8f861ca284500c3c34
Scott Meadows <Scottmeadows04@gmail.com>
2023-01-18 13:00:49 -0500

    Add show action

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,4 +2,11 @@ class BirdsController < ApplicationController
   def index
     render json: Bird.all, status: :ok
   end
+
+  def show
+    bird = Bird.find(params[:id])
+    render json: bird, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: "Bird not found", status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # resources :birds
+  resources :birds, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "birds#index"
 end
